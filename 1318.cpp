b905a78576ba5f255e1e5b539f4c90bb8e3dac20
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-09-26 00:06:27 +0530

    Create 1318.cpp

---
+++ 1318.cpp
@@ -0,0 +1,18 @@
+// 1318. Minimum Flips to Make a OR b Equal to c
+
+
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int res{0};
+        while(a || b || c) {
+            if ((c&1) == 1)
+                res += ((a&1)==0)&&((b&1)==0);
+            else
+                res += (a&1) + (b&1);
+            a>>=1; b>>=1; c>>=1;
+        }
+        return res;
+    }
+};
