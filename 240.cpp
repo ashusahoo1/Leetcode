1584750d37e5fd2dd667a337606419a053e0c043
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-09-29 18:07:43 +0530

    Create 240.cpp

---
+++ 240.cpp
@@ -0,0 +1,19 @@
+//240. Search a 2D Matrix II
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int rowIndex = 0;
+        int colIndex = col - 1;
+        while(rowIndex < row && colIndex >= 0){
+            int ele = matrix[rowIndex][colIndex];
+            if(ele == target) return 1;
+            if(ele < target) rowIndex++;  // Move downward
+            else colIndex--; // Shift left
+        }
+        return 0;
+    }
+};
