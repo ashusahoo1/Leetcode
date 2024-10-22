e5e77be639070628af14b0fdffd4e7399dbe8c84
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-10-22 23:05:19 +0530

    Create 171.cpp

---
+++ 171.cpp
@@ -0,0 +1,14 @@
+// 171. Excel Sheet Column Number
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int n = columnTitle.length();
+        int sum = (((int)(columnTitle[0]-'A'))+1);
+        for(int i=1;i<n;i++){
+            sum *= 26;
+            sum += ((int)(columnTitle[i]-'A')+1);
+        }
+        return sum;
+    }
+};
