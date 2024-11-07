23c7f0f39259aa56ead25fe68c2a7194a9e56cfa
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-11-07 16:36:42 +0530

    Create 303.cpp

---
+++ 303.cpp
@@ -0,0 +1,16 @@
+// 303. Range Sum Query - Immutable
+
+class NumArray {
+public:
+    vector<int> ans;
+    NumArray(vector<int>& nums) {
+        ans.push_back(0);
+        for(int num:nums){
+            ans.push_back(ans.back()+num);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return ans[right+1]-ans[left];
+    }
+};
