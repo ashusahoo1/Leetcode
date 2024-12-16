2aa625f8d99a745ce0746fabafd7508092fbdb89
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-12-16 18:58:36 +0530

    Create 3624.cpp

---
+++ 3624.cpp
@@ -0,0 +1,25 @@
+//3264. Final Array State After K Multiplication Operations I
+
+
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+      
+        for(int i = 0; i<k; i++){
+            int miniElement = INT_MAX;
+            int index = -1;
+            for(int j =0; j<nums.size();j++){
+                if(miniElement > nums[j]) {
+                    miniElement  = nums[j];
+                    index = j;
+                }
+                
+            }
+            nums[index] = nums[index] * multiplier;
+            
+            
+        }
+        
+        return nums;
+    }
+};
