7ce6f7394b0b4b19074efc01c350fe07da26e273
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2024-12-17 19:43:43 +0530

    Create 1534.cpp

---
+++ 1534.cpp
@@ -0,0 +1,25 @@
+//1534. Count Good Triplets
+
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int ans = 0;
+        int n = arr.size();
+        
+        // Iterate over all possible triplets (i, j, k) with i < j < k
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    // Check the conditions for good triplets
+                    if (abs(arr[i] - arr[j]) <= a &&
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
