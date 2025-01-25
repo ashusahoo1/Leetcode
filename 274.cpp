cf96c8376a728fd2f16dbf4ea5f5bb13a3dc7e6c
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2025-01-25 16:21:37 +0530

    Update 274.cpp

--- 274.cpp
+++ 274.cpp
@@ -1,1 +1,32 @@
 // 274 Hindex
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        
+        int n = citations.size();
+        if(n==0)
+            return 0;
+        if(citations[0]>=n)
+            return n;
+        int high = n-1;
+        int low=0;
+        int mid;
+        int candidate = 0;
+        
+        while(low<=high)
+        {
+            mid=(high+low)/2;
+            if(citations[mid]>=n-mid)
+            {
+                candidate = n-mid;
+                high = mid-1;
+            }
+            else
+                low = mid + 1;
+        }
+        return candidate;
+    }
+};
