288b6b48ee4a9df9e3ea0f63b6ac304b20561aaa
ashusahoo1 <155668350+ashusahoo1@users.noreply.github.com>
2025-05-15 01:26:25 +0530

    Create 412.cpp

---
+++ 412.cpp
@@ -0,0 +1,24 @@
+//412. Fizz Buzz
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+       vector<string>ans(n);
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0&&i%5==0)
+              ans[i-1]="FizzBuzz";
+            else if(i%3==0)
+              ans[i-1]="Fizz";
+            else if(i%5==0)
+              ans[i-1]="Buzz";
+            else
+            {
+              string s=to_string(i);
+              ans[i-1]=s;
+            }
+              
+        }
+        return ans;
+    }
+};
